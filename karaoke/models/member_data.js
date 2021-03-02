66f28826f95e4abb853b8c8680cb78bb63b7707f
Laura Bustamante <laurabustamante@Lauras-2017-MacBook-Pro.local>
2021-03-02 14:37:24 -0500

    connected app to mongoose and test schema

---
+++ karaoke/models/member_data.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const songSchema = new mongoose.Schema({
+    artist: { type: String, required: true },
+    title: { type: String, required: true },
+    genre: { type: String, required: true },
+    songId: { type: String, required: true }
+});
+
+const memberSchema = new mongoose.Schema({
+    name:  { type: String, required: true },
+    nickname:  { type: String, required: true },
+    city: { type: String, required: true },
+    faveGenre: { type: String, required: true },
+    drinkOrder: { type: String, required: true },
+    songList: [songSchema] // set default to empty string?
+});
+
+const Member = mongoose.model('Member', memberSchema);
+
+module.exports = Member;
--- karaoke/server.js
+++ karaoke/server.js
@@ -1,12 +1,21 @@
 // DEPENDENCIES
 const express = require('express'); // import express web framework for node.js
+const mongoose = require('mongoose'); // import to use mongoose db
+const Member = require('./models/member_data.js');
 
 // CONFIGURATION
 const app = express(); // create a shorthand for the express functions;
 const port = 3000;
+const databaseName = 'karaokeDb'; // name of mongoose database
 
 // MIDDLEWARE
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
+
+// DATABASE
+mongoose.connect('mongodb://localhost:27017/', { dbName: databaseName }, { useNewUrlParser: true}); // this sets the database name in mongoose and the data format
+mongoose.connection.once('open', ()=> {
+    console.log('Karaoke Clubhouse connected to mongo'); // this is a test line that tells us that it connected successfully
+});
 
 // ROUTES 
 
@@ -20,6 +29,14 @@ app.get('/', (req, res) => { // this is what the app sends when it is requested
 app.get('/lobby', (req, res) => { // app sends lobby.ejs html render when accessing /lobby
     // res.send('Hello World! This is Karaoke Clubhouse Lobby!') // use to test before testing .ejs render;
     res.render('lobby.ejs');
+});
+
+// create --> new member post route
+app.post('/lobby/', (req, res)=>{
+    // add any data handling needed to change post data to match database schema
+    Member.create(req.body, (error, createdData)=>{
+        res.send(createdData);
+    });
 });
 
 // LISTENER, express is listening to port 3000
